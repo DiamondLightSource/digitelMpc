9d9d7503bf21003086922c2ceaf3b179d5b0d216
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2010-01-08 16:04:13 +0000

    digitelMpc: simplified builder interface, and added macro descriptions and defaults to templates

--- etc/builder.py
+++ etc/builder.py
@@ -1,83 +1,57 @@
-from iocbuilder import Substitution, SetSimulation
+from iocbuilder import AutoSubstitution, Substitution, SetSimulation, ModuleBase
+from iocbuilder.modules.streamDevice import AutoProtocol
 from iocbuilder.arginfo import *
 
-from iocbuilder.modules.streamDevice import AutoProtocol
-from iocbuilder.modules.asyn import AsynOctetInterface
-
-class digitelMpc(Substitution, AutoProtocol):
-    '''Digitel MPC Ion pump controller'''
-
+class digitelMpc(AutoSubstitution):
     # Make sure unit is a 2 digit int
-    def __init__(self, device, port, unit = 1):
-        unit = "%02d" % unit
-        self.__super.__init__(**filter_dict(locals(), self.Arguments))
-
-    # __init__ arguments
-    ArgInfo = makeArgInfo(__init__,
-        device = Simple('Device Prefix', str),
-        port   = Ident ('Asyn Serial Port', AsynOctetInterface),
-        unit   = Simple('Unit number (for multidrop serial)', int))
-
+    def __init__(self, **args):
+        if "unit" in args:
+            args["unit"] = "%02d" % int(args["unit"])
+        self.__super.__init__(**args)
+        
     # Substitution attributes
     TemplateFile = 'digitelMpc.template'
-    Arguments = ArgInfo.Names()
     
     # AutoProtocol attributes
-    ProtocolFiles = ['digitelMpc.proto']    
+    ProtocolFiles = ['digitelMpc.proto']
 
 # Simulation has a different db
-class digitelMpc_sim(digitelMpc):
-    TemplateFile = 'simulation_digitelMpc.template'
+class digitelMpc_sim(Substitution):
+    TemplateFile = 'simulation_digitelMpc.template'        
 SetSimulation(digitelMpc, digitelMpc_sim)
 
-class digitelMpcPump(Substitution):
+class digitelMpcPump(ModuleBase):
     pass
         
-class digitelMpcTsp(digitelMpcPump):
-    Arguments = ('device', 'port' , 'unit' , 'ctlsrc')
+class digitelMpcTsp(AutoSubstitution, digitelMpcPump):
     TemplateFile = 'digitelMpcTsp.template'
 
-class digitelMpcIonp(digitelMpcPump):
-    '''Digitel MPC Ion pump template'''
+class _digitelMpcIonpTemplate(AutoSubstitution):
+    TemplateFile = 'digitelMpcIonp.template'    
 
+class digitelMpcIonp(_digitelMpcIonpTemplate, digitelMpcPump):
+    __doc__ = _digitelMpcIonpTemplate.__doc__
     # Just pass the arguments straight through
-    def __init__(self, device, MPC, pump, size, spon = 0, spoff = 0, name = ""):
-        port = MPC.args['port']
-        unit = MPC.args['unit']
-        self.__super.__init__(**filter_dict(locals(), self.Arguments))
+    def __init__(self, MPC, **args):
+        args['port'] = MPC.args['port']
+        args['unit'] = MPC.args['unit']
+        self.__super.__init__(**args)
 
     # __init__ arguments
     ArgInfo = makeArgInfo(__init__,
-        device = Simple('Device Prefix', str),
-        MPC    = Ident ('digitelMPC object', digitelMpc),
-        pump   = Simple('Pump number', int),
-        size   = Simple('Pump size (l)', int),
-        spon   = Simple('Setpoint for on', int),
-        spoff  = Simple('Setpoint for off', int),
-        name   = Simple('Object name', str))
-
-    # Substitution attributes
-    Arguments = ArgInfo.Names(without = ['MPC']) + ['port', 'unit']
-    TemplateFile = 'digitelMpcIonp.template'
+        MPC    = Ident ('digitelMPC object', digitelMpc)) + \
+        _digitelMpcIonpTemplate.ArgInfo.filtered(without = ['port', 'unit'])
 
 # Simulation has a different db
 class digitelMpcIonp_sim(digitelMpcIonp):
     TemplateFile = 'simulation_digitelMpcIonp.template'
 SetSimulation(digitelMpcIonp, digitelMpcIonp_sim)
             
-class digitelMpcIonpGroup(Substitution):
-    Arguments = (
-        'device', 'delay',
-        'ionp1', 'ionp2', 'ionp3', 'ionp4',
-        'ionp5', 'ionp6', 'ionp7', 'ionp8')
+class digitelMpcIonpGroup(AutoSubstitution):
     TemplateFile = 'digitelMpcIonpGroup.template'
 
-class digitelMpcTspGroup(Substitution):
-    Arguments = (
-        'device',
-        'tsp1', 'tsp2', 'tsp3', 'tsp4', 'tsp5', 'tsp6', 'tsp7', 'tsp8')
+class digitelMpcTspGroup(AutoSubstitution):
     TemplateFile = 'digitelMpcTspGroup.template'
 
-class dummyIonp(Substitution):
-    Arguments = ('device',)
+class dummyIonp(AutoSubstitution):
     TemplateFile = 'dummyIonp.template'
