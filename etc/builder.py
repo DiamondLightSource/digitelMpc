e5734a4cf995adf0d85512508084faff9fd7da34
Lee Hudson <lee.hudson@diamond.ac.uk>
2022-04-01 10:15:00 +0100

    Modified builder so the proto and unit are passed to the digitelMpc template

--- etc/builder.py
+++ etc/builder.py
@@ -1,4 +1,4 @@
-from iocbuilder import AutoSubstitution, Substitution, SetSimulation, ModuleBase
+from iocbuilder import AutoSubstitution, Substitution, SetSimulation, ModuleBase, Device
 from iocbuilder.modules.streamDevice import AutoProtocol
 from iocbuilder.arginfo import *
 from iocbuilder.modules.calc import Calc
@@ -7,25 +7,31 @@ from iocbuilder.modules.asyn import AsynIP
 class digitelMpcPump(ModuleBase):
     pass
 
-class _digitelMpcTemplate(AutoSubstitution,AutoProtocol ):
+class _digitelMpcTemplate(AutoSubstitution ):
     TemplateFile = 'digitelMpc.template'
 
-class digitelMpc(_digitelMpcTemplate,digitelMpcPump):
+class digitelMpc(digitelMpcPump, AutoProtocol):
     # Make sure unit is a 2 digit int
-    def __init__(self,unit = "01", proto = "digitelMpc.proto",**args):
-        self.__super.__init__(**args)
-    # Dependencies
+
     Dependencies = (Calc, )
-
-
-    # AutoProtocol attributes
     ProtocolFiles = ['digitelMpc.proto']
 
+    def __init__(self,name,device,port, unit = "01", proto = "digitelMpc.proto"):
+        self.__super.__init__()
+        self.name = name
+        self.device = device
+        self.port = port
+        self.unit = unit
+        self.proto = proto
+        _digitelMpcTemplate(device=self.device,port=self.port,unit=self.unit,proto=self.proto)
+
     ArgInfo = makeArgInfo(__init__,
+        name = Simple("Gui tag",str),
+        device = Simple("Device name",str),
         port = Ident("Asyn port",AsynIP),
         unit = Choice("Unit number for multi drop serial",["%02d" % unit for unit in range(1,33)]),
-        proto = Choice("Protocol file to use", ["digitelMpc.proto","digitelMpcq.proto"])) + \
-        _digitelMpcTemplate.ArgInfo.filtered(without = ['a', 'c','port','unit','proto'])
+        proto = Choice("Protocol file to use", ["digitelMpc.proto","digitelMpcq.proto"]),
+    )
     
    
 
@@ -42,8 +48,8 @@ class digitelMpcIonp(_digitelMpcIonpTemplate, digitelMpcPump):
     __doc__ = _digitelMpcIonpTemplate.__doc__
     # Just pass the arguments straight through
     def __init__(self, MPC, **args):
-        args['port'] = MPC.args['port']
-        args['unit'] = MPC.args['unit']
+        args['port'] = MPC.port
+        args['unit'] = MPC.unit
         self.__super.__init__(**args)
 
     # __init__ arguments
