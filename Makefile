6f5e0157ef30767be5f7fa97c8eb5ea6fe496391
Giles Knap <giles.knap@diamond.ac.uk>
2024-05-24 08:33:18 +0000

    sanitise for external consumption

--- Makefile
+++ Makefile
@@ -6,23 +6,23 @@ include $(TOP)/configure/CONFIG
 DIRS := $(DIRS) $(filter-out $(DIRS), configure)
 #DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard *App))
 #DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard *app))
-DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocBoot))
-DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocboot))
+# DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocBoot))
+# DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocboot))
 DIRS += digitelMpcApp
 
-## Uncomment the following to build the simulation and example apps 
+## Uncomment the following to build the simulation and example apps
 ## (NB make sure configure/RELEASE is updated too)
 #DIRS += exampleApp
-DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard etc))
+# DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard etc))
 # Comment out the following line to disable building of example iocs
-DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocs))
+# DIRS := $(DIRS) $(filter-out $(DIRS), $(wildcard iocs))
 
 
 include $(TOP)/configure/RULES_TOP
 
 clean:
 	rm -rf $(TOP)/data
-	
+
 else
 
 include $(TOP)/config/CONFIG_APP
--- digitelMpcApp/protocol/generateMpcqProto.py
+++ digitelMpcApp/protocol/generateMpcqProto.py
@@ -1,4 +1,4 @@
-#!/dls_sw/prod/tools/RHEL7-x86_64/defaults/bin/dls-python3.7
+#!env python
 
 # This script generates the digitelMpcq.proto (MPCq) file by taking all functions from digitelMpc.proto (MPCe) as a base function set.
 # It then reads the digitelMpcq.override file and replaces any functions from digitelMpc.proto with the version found in digitelMpcq.override.
@@ -11,11 +11,11 @@
 
 import os
 from collections import OrderedDict
-from subprocess import Popen, PIPE
+from subprocess import PIPE, Popen
+
 
 def readProtocol(protocolFile):
-
-    inputFile = open(protocolFile,"r")
+    inputFile = open(protocolFile, "r")
     functions = OrderedDict()
     parameters = list()
     function = ""
@@ -24,47 +24,53 @@ def readProtocol(protocolFile):
     closeParenthesesCount = 0
 
     for line in inputFile:
-        if(line[0][0] != '#' and line[0][0] != '\n'):
-            if(inFunction == False and '{' not in line):
+        if line[0][0] != "#" and line[0][0] != "\n":
+            if inFunction == False and "{" not in line:
                 parameters.append(line)
-            if('{' in line and inFunction == False):
+            if "{" in line and inFunction == False:
                 inFunction = True
-                openParenthesesCount += line.count('{')
-                closeParenthesesCount += line.count('}')
-                function+=line
+                openParenthesesCount += line.count("{")
+                closeParenthesesCount += line.count("}")
+                function += line
 
-            elif(inFunction):
-                openParenthesesCount += line.count('{')
-                closeParenthesesCount += line.count('}')
-                function+=line
-                    
-            if(openParenthesesCount == closeParenthesesCount and inFunction == True):
+            elif inFunction:
+                openParenthesesCount += line.count("{")
+                closeParenthesesCount += line.count("}")
+                function += line
+
+            if openParenthesesCount == closeParenthesesCount and inFunction == True:
                 inFunction = False
                 openParenthesesCount = 0
                 closeParenthesesCount = 0
                 methodName = function.split()[0]
-                functions[methodName]=function[len(methodName):]
+                functions[methodName] = function[len(methodName) :]
                 function = ""
 
-    return functions,parameters
-                
+    return functions, parameters
+
+
 # Read in the digitelMpc.proto file. This will be the basis for the digitelMpcq.proto file.
-baseFunctions,baseParam = readProtocol("digitelMpc.proto")
+baseFunctions, baseParam = readProtocol("digitelMpc.proto")
 
 # Find file  with the string "override" in them
-overrideFiles = Popen("ls | grep '.override'",shell=True,stdout=PIPE).stdout.read().decode().split('\n')[:-1]
+overrideFiles = (
+    Popen("ls | grep '.override'", shell=True, stdout=PIPE)
+    .stdout.read()
+    .decode()
+    .split("\n")[:-1]
+)
 
 for overrideFile in overrideFiles:
     # Read in Override file
-    overrideFunctions,parameters = readProtocol(overrideFile)
+    overrideFunctions, parameters = readProtocol(overrideFile)
     # Replace functions in baseFunctions if they appear in the override file
     for overrideFunction in overrideFunctions:
-        if(overrideFunction in baseFunctions):
-            baseFunctions[overrideFunction]=overrideFunctions[overrideFunction]
-    
+        if overrideFunction in baseFunctions:
+            baseFunctions[overrideFunction] = overrideFunctions[overrideFunction]
+
     # Create protocol file
-    outputFileName = overrideFile.replace('.override','.proto')
-    outputProto = open(outputFileName,'w')
+    outputFileName = overrideFile.replace(".override", ".proto")
+    outputProto = open(outputFileName, "w")
 
     outputProto.write(f"# File was generated from {__file__}, do not modify \n")
 
@@ -72,11 +78,11 @@ for overrideFile in overrideFiles:
         outputProto.write(parameter)
     for function in baseFunctions:
         outputProto.write(function + baseFunctions[function])
-        outputProto.write('\n')
+        outputProto.write("\n")
     for overrideFunction in overrideFunctions:
         if overrideFunction not in baseFunctions:
             outputProto.write(overrideFunction + overrideFunctions[overrideFunction])
-            outputProto.write('\n')
+            outputProto.write("\n")
 
     outputProto.close()
-    os.system('mv %s ../../data'%outputFileName)
+    os.system("mv %s ../../data" % outputFileName)
